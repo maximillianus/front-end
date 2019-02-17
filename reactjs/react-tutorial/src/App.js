ec1abc9d46165a64a4e3dc0ee51f96dfacd98751
maximillianus <b4nd1c00t@yahoo.com>
2019-02-17 19:49:53 +0700

    tania rascia react tutorial done

--- reactjs/react-tutorial/src/App.js
+++ reactjs/react-tutorial/src/App.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Table from './Table';
+import Form from './Form';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        characters: [
+            {
+                'name': 'Charlie',
+                'job': 'Janitor'
+            },
+            {
+                'name': 'Mac',
+                'job': 'Bouncer'
+            },
+            {
+                'name': 'Dee',
+                'job': 'Aspring actress'
+            },
+            {
+                'name': 'Dennis',
+                'job': 'Bartender'
+            },
+            {
+                'name': 'Edgar',
+                'job': 'Waitress'
+            }
+        ]
+    };
+
+    removeCharacter = index => {
+        const { characters } = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            })
+        });
+    }
+
+    handleSubmit = character => {
+        this.setState({characters: [...this.state.characters, character]});
+    }
+    render() {
+        const { characters } = this.state;
+        
+        return (
+            <div className="container">
+                <h1>Hello, React!</h1>
+                <Table 
+                    characterData={characters}
+                    removeCharacter={this.removeCharacter}
+                />
+                <Form 
+                    handleSubmit = {this.handleSubmit}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
